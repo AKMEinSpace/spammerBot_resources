e96109661666f27155b6f6732a5e7e6ac7e55cfb
AKMEinSpace <60017392+AKMEinSpace@users.noreply.github.com>
2021-04-22 14:58:06 +0200

    Add files via upload
    finished code and some good old-fashioned movie scripts to get you started..

---
+++ spammerBot.py
@@ -0,0 +1,15 @@
+import pyautogui, time
+
+f = open("bee.txt", "r")
+content = f.readlines()
+
+def start(content):
+    for line in content:
+        pyautogui.typewrite(line.strip())
+        pyautogui.press("enter")
+
+        time.sleep(0.2)
+
+
+input("Press enter to start the program!")
+start()
